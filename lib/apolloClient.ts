7bd201130931f9d3abc702889b386e16e9e3a40b
yukiHaga <oceansthirteen7510@gmail.com>
2022-09-12 00:16:14 +0900

    Add: client comment

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -7,6 +7,7 @@ import {
 
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
 
+// NOTICE: Nextでこう書くのはあまり推奨されていないそう。
 export const client = new ApolloClient({
   uri: "https://flyby-gateway.herokuapp.com/",
   cache: new InMemoryCache(),
